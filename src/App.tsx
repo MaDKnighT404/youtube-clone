e1b0417a5c768147d4cdedf6c62c43395f96581b
George Koloidi <Battleaxe@bk.ru>
2023-03-08 23:09:55 +0100

    feat: add routing between 3 pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/watch/:id" element={<Watch />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
