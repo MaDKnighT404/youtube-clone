d58f0334c6efaf807b38dbf31cfc05a346fecc83
George Koloidi <Battleaxe@bk.ru>
2023-03-10 13:30:47 +0100

    feat: add Watch page and WatchCard

--- src/redux/store/Slices/YoutubeSlice.ts
+++ src/redux/store/Slices/YoutubeSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { InitialState } from '../../../Types';
 import { getHomePageVideos } from '../reducers/getHomePageVideos';
+import { getRecommendedVideos } from '../reducers/getRecommendedVideos';
 import { getSearchPageVideos } from '../reducers/getSearchPageVideos';
+import { getVideoDetails } from '../reducers/getVideoDetails';
 const initialState: InitialState = {
   videos: [],
   currentPlaying: null,
@@ -34,6 +36,12 @@ export const YoutubeSlice = createSlice({
     builder.addCase(getSearchPageVideos.fulfilled, (state, action) => {
       state.videos = action.payload.parsedData;
       state.nextPageToken = action.payload.nextPageToken
+    });
+    builder.addCase(getVideoDetails.fulfilled, (state, action) => {
+      state.currentPlaying = action.payload;
+    });
+    builder.addCase(getRecommendedVideos.fulfilled, (state, action) => {
+      state.recommendedVideos = action.payload.parsedData;
     })
   },
 });
